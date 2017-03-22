6e68d749cba3f116d98745911d4e791ce0eadd93
sybrendekloet <sybrendekloet@gmail.com>
2017-03-22 15:51:28 +0100

    Delete get_triggerTimes.m

--- get_triggerTimes.m
+++
@@ -1,8 +0,0 @@
-function [trig]=get_triggerTimes(ADCchannel)
-% function [TTLts]=get_triggerTimes(ADCchannel) will return timestamps on
-% which the TTL was up (from 200ms piezo protocol)
-
-ADChigh=find(ADCchannel<2);ADCd=diff(ADChigh);
-firstHigh=find(ADCd>300);
-trig=[ADChigh(1); ADChigh(firstHigh+1)]; %timestamp in samples
-
